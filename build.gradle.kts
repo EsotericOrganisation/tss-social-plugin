6b8f4f5af65ca374a32825b1dff85a126ce84954
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-17 19:17:19 +0100

    📋 Configured gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    java
+    application
+
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.1.0"
+}
+
+group = "net.slqmy"
+version = "0.1"
+description = "The plugin to manage social aspects of The Slimy Swamp, such as the messaging system, guilds and parties."
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(files("../TSS-Core/build/libs/tss_core-0.1-dev-all.jar"))
+    compileOnly(files("../TSS-Ranks/build/libs/tss_ranks-0.1-dev.jar"))
+
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        val props = mapOf(
+                "name" to project.name,
+                "version" to project.version,
+                "description" to project.description,
+                "apiVersion" to "1.20"
+        )
+
+        inputs.properties(props)
+
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.20.1")
+    }
+}
+
+application {
+    mainClass.set("TSSSocialPlugin")
+}
